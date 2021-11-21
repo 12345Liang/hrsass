b5375be445748e0c654de5d137f75d5e48fb049e
lsq <2268518651@qq.com>
2021-11-21 19:34:55 +0800

    提交成功

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,11 @@
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 import { getToken, setToken, removeToken } from '@/utils/auth'
 const state = {
   // 初始化的时候从缓存中读取状态 并赋值到初始化的状态上
-  token: getToken()
+  token: getToken(),
   // 设置token初始状态   token持久化 => 放到缓存中
   // Vuex和前端缓存相结合实现Vuex的持久化
+  userInfo: {} // 定义一个空的对象 不是null 因为后边我要开发userInfo的属性给别人用  userInfo.name
 }
 const mutations = {
   setToken(state, token) {
@@ -16,12 +17,24 @@ const mutations = {
   removeToken(state) {
     state.token = null// 删除vuex的token
     removeToken()// 先清除 vuex  再清除缓存 vuex和 缓存数据的同步
+  },
+  setUserInfo(state, userInfo) {
+    state.userInfo = { ...userInfo } // 用 浅拷贝的方式去赋值对象 因为这样数据更新之后，才会触发组件的更新
+  },
+  // 删除用户信息
+  reomveUserInfo(state) {
+    state.userInfo = {}
   }
 }
 const actions = {
   async login(context, data) {
     const result = await login(data)
     context.commit('setToken', result)
+  },
+  async getUserInfo(context) {
+    const result = await getUserInfo() // 获取返回值
+    context.commit('setUserInfo', result) // 将整个的个人信息设置到用户的vuex数据中
+    return result // 这里为什么要返回 为后面埋下伏笔
   }
 }
 export default {
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,19 @@
 import axios from 'axios'
+import store from '@/store'
 import { Message } from 'element-ui'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
-// request interceptor
+service.interceptors.request.use(config => {
+  if (store.getters.token) {
+    config.headers['Authorization'] = `Bearer ${store.getters.token}`
+  }
+  return config
+}, error => {
+  return Promise.reject(error)
+})
 service.interceptors.response.use(response => {
   const { success, message, data } = response.data
   //   要根据success的成功与否决定下面的操作
